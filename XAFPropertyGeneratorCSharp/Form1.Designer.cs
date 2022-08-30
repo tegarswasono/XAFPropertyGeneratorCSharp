daf4db3cdb1c48946d33831b74b06bb4f9a088d7
tegarswasono <tegar.s@weefer.co.id>
2022-08-30 10:50:52 +0700

    first commit

---
+++ XAFPropertyGeneratorCSharp/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿
+namespace XAFPropertyGeneratorCSharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtbSource = new System.Windows.Forms.RichTextBox();
+            this.bGenerate = new System.Windows.Forms.Button();
+            this.rtbDestination = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // rtbSource
+            // 
+            this.rtbSource.Location = new System.Drawing.Point(41, 51);
+            this.rtbSource.Name = "rtbSource";
+            this.rtbSource.Size = new System.Drawing.Size(917, 273);
+            this.rtbSource.TabIndex = 0;
+            this.rtbSource.Text = "";
+            // 
+            // bGenerate
+            // 
+            this.bGenerate.Location = new System.Drawing.Point(41, 356);
+            this.bGenerate.Name = "bGenerate";
+            this.bGenerate.Size = new System.Drawing.Size(917, 34);
+            this.bGenerate.TabIndex = 1;
+            this.bGenerate.Text = "Generate";
+            this.bGenerate.UseVisualStyleBackColor = true;
+            this.bGenerate.Click += new System.EventHandler(this.bGenerate_Click);
+            // 
+            // rtbDestination
+            // 
+            this.rtbDestination.Location = new System.Drawing.Point(41, 428);
+            this.rtbDestination.Name = "rtbDestination";
+            this.rtbDestination.Size = new System.Drawing.Size(917, 273);
+            this.rtbDestination.TabIndex = 2;
+            this.rtbDestination.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1007, 768);
+            this.Controls.Add(this.rtbDestination);
+            this.Controls.Add(this.bGenerate);
+            this.Controls.Add(this.rtbSource);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtbSource;
+        private System.Windows.Forms.Button bGenerate;
+        private System.Windows.Forms.RichTextBox rtbDestination;
+    }
+}
+
---
+++ XAFPropertyGeneratorCSharp/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XAFPropertyGeneratorCSharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "XAF C# Property Generator";
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+        }
+
+        private void bGenerate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string[] properties = rtbSource.Text.Split("\n");
+
+                //constructor
+                string[] classDetail = properties[0].Split(" ");
+                string className = classDetail[0];
+
+                string generatedClassHeader = $"[DefaultClassOptions]" + Environment.NewLine;
+                generatedClassHeader += $"public class {className} : BaseObject" + Environment.NewLine + "{" + Environment.NewLine;
+                string generatedClassConstructor = $"\t public {className}(Session session) : base(session) {{}}";
+                string generatedClassFooter = $"}}";
+
+                List<string> generatedProperties = new List<string>();
+                int index = -1;
+                foreach (string property in properties)
+                {
+                    index += 1;
+                    if (index == 0)
+                    {
+
+                    }
+                    else
+                    {
+                        string[] propertyDetail = property.Split(" ");
+                        string propertyName = propertyDetail[0];
+                        string propertyType = propertyDetail[1];
+
+                        string privatePropertyName = "_" + propertyName.Substring(0, 1).ToLower() + propertyName.Substring(1, propertyName.Length - 1);
+                        string generatedProperty = $"\t private {propertyType} {privatePropertyName};";
+                        if (index % 5 == 0) generatedProperty += Environment.NewLine;
+
+                        generatedProperties.Add(generatedProperty);
+                    }
+                }
+
+                index = -1;
+                foreach (string property in properties)
+                {
+                    index += 1;
+                    if (index == 0)
+                    {
+
+                    }
+                    else
+                    {
+                        string[] propertyDetail = property.Split(" ");
+                        string propertyName = propertyDetail[0];
+                        string propertyType = propertyDetail[1];
+
+                        string privatePropertyName = "_" + propertyName.Substring(0, 1).ToLower() + propertyName.Substring(1, propertyName.Length - 1);
+                        string generatedProperty = "";
+                        generatedProperty += $"\t public {propertyType} {propertyName} {{" + Environment.NewLine;
+                        generatedProperty += $"\t \t get {{ return {privatePropertyName};}}" + Environment.NewLine;
+                        generatedProperty += $"\t \t set {{ SetPropertyValue(nameof({propertyName}), ref {privatePropertyName}, value);}}" + Environment.NewLine;
+                        generatedProperty += $"\t }}";
+
+                        generatedProperties.Add(generatedProperty);
+                    }
+                }
+
+                rtbDestination.Text = generatedClassHeader + generatedClassConstructor + Environment.NewLine;
+                foreach (string generatedProperty in generatedProperties)
+                {
+                    rtbDestination.Text += (generatedProperty + Environment.NewLine);
+                }
+                rtbDestination.Text += generatedClassFooter;
+            }
+            catch (Exception ex)
+            {
+                rtbDestination.Text = "Error | " + ex.ToString();
+            }
+        }
+    }
+}
